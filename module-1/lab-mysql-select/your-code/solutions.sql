2647bbb855c2ce4623b24f244085a7bb64258edc
carlosmd14 <carlosmadrizd@gmail.com>
2019-01-23 16:23:34 +0100

    Subiendo lab-mysql-select hasta Challenge 4

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,55 @@
+#Challenge 1
+
+SELECT authors.au_id AS AUTHOR_ID, authors.au_lname AS LAST_NAME, authors.au_fname AS FIRST_NAME, titles.title AS TITLE, publishers.pub_name AS PUBLISHER
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id=titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id=titles.title_id
+INNER JOIN publishers
+ON titles.pub_id=publishers.pub_id;
+
+#Challenge 2
+
+SELECT authors.au_id AS AUTHOR_ID, 
+authors.au_lname AS LAST_NAME, 
+authors.au_fname AS FIRST_NAME, 
+COUNT(titles.title_id) AS TITLE_COUNT, 
+publishers.pub_name AS PUBLISHER
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id=titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id=titles.title_id
+INNER JOIN publishers
+ON titles.pub_id=publishers.pub_id
+GROUP BY authors.au_id, publishers.pub_name;
+
+#Challenge 3
+
+SELECT authors.au_id AS AUTHOR_ID, 
+authors.au_lname AS LAST_NAME, 
+authors.au_fname AS FIRST_NAME, 
+SUM(titles.ytd_sales) AS TOTAL
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id=titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id=titles.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(titles.ytd_sales) DESC
+LIMIT 3;
+
+#Challenge 4
+
+SELECT authors.au_id AS AUTHOR_ID,
+authors.au_lname AS LAST_NAME, 
+authors.au_fname AS FIRST_NAME, 
+IFNULL(SUM(titles.ytd_sales),0) AS TOTAL
+FROM authors
+LEFT JOIN titleauthor
+ON authors.au_id=titleauthor.au_id
+LEFT JOIN titles
+ON titleauthor.title_id=titles.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
